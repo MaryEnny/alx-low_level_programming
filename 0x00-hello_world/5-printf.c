862d3d33cdf4cbf1c683e01707ce1781fb23c089
MaryEnny <eniolamary37@gmail.com>
2023-08-14 14:38:25 -0700

    size.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main entrance
+ *
+ * Return: this should be 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
